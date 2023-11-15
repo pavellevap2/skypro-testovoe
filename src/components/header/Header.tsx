db7fa819ffefcef743399594afdd90f98814c4a5
Одинцов Павел Владимирович <P.Odintsov@otpbank.ru>
2023-11-15 13:05:55 +0300

    feat: add desktop header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,15 @@
 import './Header.scss';
 
 const Header = () => {
-  return <div className="Header">Интерьер.</div>;
+  return (
+    <header className="header">
+      <div className="header-title">Интерьер.</div>
+      <div className="header-nav">
+        <div className="header-nav-item">Каталог</div>
+        <div className="header-nav-item">Корзина</div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
